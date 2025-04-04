28917b8a2011f44902724a4ff504d73a437d03c4
Saksham Srivastava <116826100+Srivattva@users.noreply.github.com>
2025-04-04 06:34:54 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+            // code readebility should be high
+console.log('Saksham')
+
+let name = "Saksham"
+let age = 19
+let isLoggedIn = true
+let state;
+
+// number => 2 to power 3
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "Saksham");
+console.log(typeof null); // null
+console.log(typeof undefined); //undefined
